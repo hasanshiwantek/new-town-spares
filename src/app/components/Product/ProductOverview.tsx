0bbd77ee477b5703a0824fc71731197cd3292163
areebhussainshiwantek <areeb.hussain@shiwantek.com>
2026-07-17 09:28:28 -0700

    Update ProductOverview.tsx

--- src/app/components/Product/ProductOverview.tsx
+++ src/app/components/Product/ProductOverview.tsx
@@ -1,126 +1,78 @@
 import Link from "next/link";
 
 const ProductOverview = ({ product }: { product: any }) => {
+
+
+  const customFields = product?.customFields?.filter(
+    (item: { name: string; value: string }) => item.name && item.value
+  )
+  console.log(product);
+
   return (
     <section className="my-8" aria-labelledby="product-overview-heading">
       <div className="w-full max-w-[1719px] flex flex-col ">
         <div className="flex flex-col gap-8">
-          {/* Main Overview Heading */}
-
-          {/* Intro Paragraph */}
-
-          {/* <div className="description-content" 
-         dangerouslySetInnerHTML={{ __html: product?.description }}>
-    </div> */}
-          {/* <p className="h5-regular !leading-relaxed w-full xl:w-[60rem] 2xl:w-[80rem]">
-            Introducing the {product?.name || "N/A"}, a versatile solution for
-            your networking needs. This product is manufactured by
-            {product?.brand?.name || "N/A"}, offering reliable performance for
-            enterprises and organizations.
-          </p> */}
-
-          {/* Key Features */}
-          {/* <section className="" aria-labelledby="key-features-heading">
-            <h3 className="h5-regular !mb-2">Key Features:</h3>
-            <ul className="!list-disc !list-inside !space-y-3">
-              <li className="h5-regular">
-                <span className="">SKU:</span> {product?.sku || "N/A"}
-              </li>
-              <li className="h5-regular">
-                <span className="">MPN:</span> {product?.mpn || "N/A"}
-              </li>
-              <li className="h5-regular">
-                <span className="">Brand:</span> {product?.brand?.name || "N/A"}
-              </li>
-              <li className="h5-regular">
-                <span className="">Category:</span>{" "}
-                {product?.categories?.[0]?.name || "N/A"}
-              </li>
-              <li className="h5-regular">
-                <span className="">Availability:</span>{" "}
-                {product?.availabilityText || "N/A"}
-              </li>
-              <li className="h5-regular">
-                <span className="">Weight:</span>{" "}
-                {product?.dimensions?.weight
-                  ? `${product.dimensions.weight} lbs`
-                  : "N/A"}
-              </li>
-            </ul>
-          </section> */}
-
-          {/* Closing Paragraph */}
-          {/* <p
-            className="!mb-10 !leading-relaxed h5-regular"
-            dangerouslySetInnerHTML={{
-              __html:
-                product?.metaDescription ||
-                product?.description ||
-                "No description available for this product.",
-            }}
-          ></p> */}
         </div>
 
         {/* Product Details Section */}
         <section className="border" aria-labelledby="product-details-heading">
-  <div className="px-[21px] py-[21px]">
-    <h2 className="text-[20px] leading-[24px] font-light text-[#333333] mb-[14px]">
-      Description
-    </h2>
+          <div className="px-[21px] py-[21px]">
+            <h2 className="text-[20px] leading-[24px] font-light text-[#333333] mb-[14px]">
+              Description
+            </h2>
 
-    <div
-  className="text-[16px] leading-[24px] text-[#333333]"
-  dangerouslySetInnerHTML={{
-    __html: product?.description ||  "No description available for this product.",
-  }}
-/>
-  </div>
+            <div
+              className="text-[16px] leading-[24px] text-[#333333]"
+              dangerouslySetInnerHTML={{
+                __html: product?.description || "No description available for this product.",
+              }}
+            />
+          </div>
 
-  {product?.customFields?.length > 0 && (
-    <>
-      <div className="px-[21px] pt-[21px]">
-        <h2 className="text-[20px] leading-[24px] font-light text-[#333333]">
-          Details
-        </h2>
-      </div>
-
-      <hr className="mx-[21px] my-3 border-t-2 border-[#333333]" />
-
-      <div className="px-[21px] pb-[21px]">
-        <dl className="space-y-2">
-          {product.customFields.map(
-            (
-              item: {
-                name: string;
-                value: string;
-              },
-              index: number
-            ) => (
-              <div
-                key={index}
-                className={`grid grid-cols-[180px_1fr] lg:grid-cols-[350px_1fr] items-center p-2 ${
-                  index % 2 === 0 ? "bg-[#F2F2F2]" : ""
-                }`}
-              >
-                <dt>{item.name}</dt>
-
-                <dd>
-                  {item.name === "Brand" && product?.brand?.name ? (
-                    <Link href={`/brand/${product.brand.slug}`}>
-                      {item.value}
-                    </Link>
-                  ) : (
-                    item.value
-                  )}
-                </dd>
+          {customFields?.length > 0 && (
+            <>
+              <div className="px-[21px] pt-[21px]">
+                <h2 className="text-[20px] leading-[24px] font-light text-[#333333]">
+                  Details
+                </h2>
               </div>
-            )
+
+              <hr className="mx-[21px] my-3 border-t-2 border-[#333333]" />
+
+              <div className="px-[21px] pb-[21px]">
+                <dl className="space-y-2">
+                  {customFields?.map(
+                    (
+                      item: {
+                        name: string;
+                        value: string;
+                      },
+                      index: number
+                    ) => (
+                      <div
+                        key={index}
+                        className={`grid grid-cols-[180px_1fr] lg:grid-cols-[350px_1fr] items-center p-2 ${index % 2 === 0 ? "bg-[#F2F2F2]" : ""
+                          }`}
+                      >
+                        <dt>{item.name}</dt>
+
+                        <dd>
+                          {item.name === "Brand" && product?.brand?.name ? (
+                            <Link href={`/brand/${product.brand.slug}`}>
+                              {item.value}
+                            </Link>
+                          ) : (
+                            item.value
+                          )}
+                        </dd>
+                      </div>
+                    )
+                  )}
+                </dl>
+              </div>
+            </>
           )}
-        </dl>
-      </div>
-    </>
-  )}
-</section>
+        </section>
       </div>
     </section>
   );
